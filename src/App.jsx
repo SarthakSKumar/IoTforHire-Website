5eb479067967b2dcecaaaf35d0a76df797a1593a
Sarthak S Kumar <sskworld9742@gmail.com>
2023-03-26 12:48:33 +0530

    revert changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,11 @@ import FAQ from "./components/FAQ.jsx";
 import Footer from "./components/Footer.jsx";
 import Contact from "./components/Contact.jsx";
 import Order from "./components/Order.jsx";
-import BookingForm from "./components/BookingForm.jsx";
-import "./index.css";
+import "./App.css";
+
 // Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
 function App() {
   const [products, setProducts] = useState("");
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
-  const handleOpenModal = () => {
-    setIsModalVisible(true);
-  };
-  const handleCloseModal = () => {
-    setIsModalVisible(false);
-  };
   const fetchData = () => {
     fetch("./assets/data.json")
       .then((response) => {
@@ -33,31 +25,13 @@ function App() {
 
   useEffect(() => {
     fetchData();
-    if (isModalVisible) {
-      document.body.style.overflow = "hidden"; // disable scrolling on html element
-    } else {
-      document.body.style.overflow = null; // re-enable scrolling on html element
-    }
-  }, [isModalVisible]);
+  }, []);
   return (
     <div className="App">
       <Order />
       <Header />
       <Hero />
       <SubHeader />
-      <div>
-        <button
-          onClick={handleOpenModal}
-          className="bg-blue-500 text-white px-4 py-2"
-        >
-          Open Modal
-        </button>
-        {isModalVisible && (
-          <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-            <BookingForm productData={products} onClose={handleCloseModal} />
-          </div>
-        )}
-      </div>
       <About />
       {products && <Catalogue productData={products} />}
       <FAQ />
--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -6,7 +6,6 @@ const BookingForm = ({ productData, onClose }) => {
     fullName: "",
     SRN: "",
     email: "",
-    section: "",
     semester: "",
     phoneNumber: "",
     dateOfReturn: "",
@@ -16,26 +15,17 @@ const BookingForm = ({ productData, onClose }) => {
   const handleSearch = (e) => setSearchText(e.target.value);
 
   const handleNameChange = (e) =>
-    setUserData({ ...userData, name: e.target.value });
-  console.log(userData);
+    setUserData({ ...userData, fullName: e.target.value });
   const handleSRNChange = (e) =>
-    setUserData({ ...userData, name: e.target.value });
-  console.log(userData);
+    setUserData({ ...userData, SRN: e.target.value });
   const handleEmailChange = (e) =>
-    setUserData({ ...userData, name: e.target.value });
-  console.log(userData);
-  const handleSectionChange = (e) =>
-    setUserData({ ...userData, name: e.target.value });
-  console.log(userData);
-  const handleSemChange = (e) =>
-    setUserData({ ...userData, name: e.target.value });
-  console.log(userData);
+    setUserData({ ...userData, email: e.target.value });
   const handlePhoneChange = (e) =>
-    setUserData({ ...userData, name: e.target.value });
-  console.log(userData);
+    setUserData({ ...userData, phoneNumber: e.target.value });
+  const handleSectionChange = (e) =>
+    setUserData({ ...userData, section: e.target.value });
   const handleDateChange = (e) =>
-    setUserData({ ...userData, name: e.target.value });
-  console.log(userData);
+    setUserData({ ...userData, dateOfReturn: e.target.value });
 
   const handleQuantityChange = (index, quantity) => {
     const newItems = [...userData.selectedItems];
@@ -70,119 +60,126 @@ const BookingForm = ({ productData, onClose }) => {
   };
 
   return (
-    <div className="p-4 bg-white h-3/4 w-2/3 overflow-scroll">
+    <div className="text-center p-4 bg-white dark:bg-gray-900 border-2 border-emerald-500 rounded-lg h-4/5 w-6/7 mx-6">
       <button
         onClick={onClose}
-        className="bg-red-500 text-white px-2 py-1 text-lg"
+        className="text-sm text-gray-300 bg-red-600 px-2 py-1 rounded-sm font-semibold"
       >
-        X
+        Go Back
       </button>
-      <input
-        type="text"
-        placeholder="Full Name"
-        value={userData.fullName}
-        onChange={handleNameChange}
-        className="border p-2 mb-4"
-      />
-      <input
-        type="text"
-        placeholder="SRN"
-        value={userData.SRN}
-        onChange={handleSRNChange}
-        className="border p-2 mb-4"
-      />
-      <input
-        type="text"
-        placeholder="Email"
-        value={userData.email}
-        onChange={handleEmailChange}
-        className="border p-2 mb-4"
-      />
-      <input
-        type="text"
-        placeholder="Section"
-        value={userData.section}
-        onChange={handleSectionChange}
-        className="border p-2 mb-4"
-      />
-      <input
-        type="text"
-        placeholder="Name"
-        value={userData.semester}
-        onChange={handleSemChange}
-        className="border p-2 mb-4"
-      />
-      <input
-        type="text"
-        placeholder="Name"
-        value={userData.phoneNumber}
-        onChange={handlePhoneChange}
-        className="border p-2 mb-4"
-      />
+      <span className="text-gray-300 text-xl font-semibold">Booking Form</span>
+      <div className="flex flex-wrap w-full justify-center md:justify-around">
+        <input
+          type="text"
+          placeholder="Full Name"
+          value={userData.fullName}
+          onChange={handleNameChange}
+          className="md:w-1/3 w-full px-4 py-2 mx-3 my-2 rounded-md focus:outline-none focus:ring-2 focus:ring-emerald-500 dark:bg-gray-800 dark:text-gray-300 dark:placeholder-gray-500"
+        />
+        <input
+          type="text"
+          placeholder="SRN"
+          value={userData.SRN}
+          onChange={handleSRNChange}
+          className="md:w-1/3 w-full px-4 py-2 mx-3 my-2 rounded-md focus:outline-none focus:ring-2 focus:ring-emerald-500 dark:bg-gray-800 dark:text-gray-300 dark:placeholder-gray-500"
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          value={userData.email}
+          onChange={handleEmailChange}
+          className="md:w-1/3 w-full px-4 py-2 mx-3 my-2 rounded-md focus:outline-none focus:ring-2 focus:ring-emerald-500 dark:bg-gray-800 dark:text-gray-300 dark:placeholder-gray-500"
+        />
+        <input
+          type="text"
+          placeholder="Section"
+          value={userData.section}
+          onChange={handleSectionChange}
+          className="md:w-1/3 w-full px-4 py-2 mx-3 my-2 rounded-md focus:outline-none focus:ring-2 focus:ring-emerald-500 dark:bg-gray-800 dark:text-gray-300 dark:placeholder-gray-500"
+        />
+        <input
+          type="text"
+          placeholder="Phone Number"
+          value={userData.phoneNumber}
+          onChange={handlePhoneChange}
+          className="md:w-1/3 w-full px-4 py-2 mx-3 my-2 rounded-md focus:outline-none focus:ring-2 focus:ring-emerald-500 dark:bg-gray-800 dark:text-gray-300 dark:placeholder-gray-500"
+        />
 
-      <input
-        type="text"
-        placeholder="Name"
-        value={userData.dateOfReturn}
-        onChange={handleDateChange}
-        className="border p-2 mb-4"
-      />
-      <input
-        type="text"
-        placeholder="Search"
-        value={searchText}
-        onChange={handleSearch}
-        className="border p-2
-mb-4"
-      />
-      <ul className="h-1/2 overflow-scroll">
-        {filteredItems.map((item) => (
-          <li
-            key={item.productName}
-            className="flex justify-between items-center mb-2"
-          >
-            <span>
-              {item.productName} - ${item.productPrice}
-            </span>
-            <button
-              onClick={() => handleSelect(item)}
-              className="bg-blue-500 text-white px-2 py-1"
+        <input
+          type="text"
+          placeholder="Date of Return"
+          value={userData.dateOfReturn}
+          onChange={handleDateChange}
+          className="md:w-1/3 w-full px-4 py-2 mx-3 my-2 rounded-md focus:outline-none focus:ring-2 focus:ring-emerald-500 dark:bg-gray-800 dark:text-gray-300 dark:placeholder-gray-500"
+        />
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchText}
+          onChange={handleSearch}
+          className="w-3/4 my-4 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-emerald-500 dark:bg-gray-800 dark:text-gray-300 dark:placeholder-gray-500"
+        />
+      </div>
+
+      <div className="w-3/4 bg-gray-800 rounded-lg inline-block overflow-y-scroll h-1/2">
+        <ul className="">
+          {filteredItems.map((item) => (
+            <li
+              key={item.productName}
+              className="flex justify-between my-2 text-gray-300 py-2 px-2"
             >
-              Select
-            </button>
-          </li>
-        ))}
-      </ul>
-      <h3 className="text-lg font-bold mb-2">Selected Items:</h3>
-      <ul>
-        {userData.selectedItems.map((item, index) => (
-          <li key={index} className="flex justify-between items-center mb-2">
-            <span>
-              {item.productName} - ${item.productPrice} x {item.quantity}
-            </span>
-            <div>
+              <img
+                src={item.productImage}
+                className="h-inherit w-10 rounded-sm inline-block"
+              />
+              <span className="w-full text-start inline-block mx-2">
+                <h6 className="text-md  font-semibold">
+                  {item.productName} - {item.productPrice}
+                </h6>
+                <h6 className="text-sm text-start">{item.productDetails}</h6>
+              </span>
               <button
-                onClick={() => decrementQuantity(index)}
-                className="bg-red-500 text-white px-2 py-1"
+                onClick={() => handleSelect(item)}
+                className="bg-blue-500 text-white px-2 py-1"
               >
-                -
+                Select
               </button>
-              <button
-                onClick={() => incrementQuantity(index)}
-                className="bg-green-500 text-white px-2 py-1"
-              >
-                +
-              </button>
-              <button
-                onClick={() => handleDelete(index)}
-                className="bg-gray-500 text-white px-2 py-1 ml-2"
-              >
-                x
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="w-1/3 block border-2 border-white overflow-y-scroll h-1/4">
+        <ul>
+          <h3 className="text-lg font-bold mb-2">Selected Items:</h3>
+          {userData.selectedItems.map((item, index) => (
+            <li key={index} className="flex justify-between items-center mb-2">
+              <span>
+                {item.productName} - ${item.productPrice} x {item.quantity}
+              </span>
+              <div>
+                <button
+                  onClick={() => decrementQuantity(index)}
+                  className="bg-red-500 text-white px-2 py-1"
+                >
+                  -
+                </button>
+                <button
+                  onClick={() => incrementQuantity(index)}
+                  className="bg-green-500 text-white px-2 py-1"
+                >
+                  +
+                </button>
+                <button
+                  onClick={() => handleDelete(index)}
+                  className="bg-gray-500 text-white px-2 py-1 ml-2"
+                >
+                  x
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
