43f620798779a802595b2b35c6b8c669ad30ce5e
Sarthak S Kumar <sskworld9742@gmail.com>
2023-04-04 21:39:03 +0530

    implement client-server

--- server/index.js
+++ server/index.js
@@ -8,6 +8,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "client", "dist", "index.html"));
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {    
   console.log(`Server listening on port ${process.env.PORT || 3000}`);
 });
